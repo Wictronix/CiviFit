155869a33717acb4e65c7f66eedcd187f72cf7d9
Vivek Poonia <86474578+ZEviod@users.noreply.github.com>
2024-10-24 21:00:06 +0530

    Update config.ts

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeaLfBtCKroiss_53E9Y4Hk54N36Hpv4Q",
-  authDomain: "civifit-b2132.firebaseapp.com",
-  projectId: "civifit-b2132",
-  storageBucket: "civifit-b2132.appspot.com",
-  messagingSenderId: "945875639397",
-  appId: "1:945875639397:web:72c7926af084ef37e87a8c"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export default firebaseConfig;
